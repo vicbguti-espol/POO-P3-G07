4c0058b77b5dc15f7e1b1dba8cf5fec9f8dab547
lolothens-e <annacast@espol.edu.ec>
2023-07-01 06:02:05 -0500

    Implementacion de clase nivel, paralelo, materia

---
+++ proyecto1_grupo07/src/main/java/com/espol/proyecto/configuracion/Nivel.java
@@ -0,0 +1,14 @@
+package main.java.com.espol.proyecto.configuracion;
+import java.util.*;
+
+public class Nivel {
+    private ArrayList<Preguntas> preguntas;
+    private Materia materia;
+    private Termino termino;
+
+    public Nivel(ArrayList pr, Materia m, Termino t){
+        this.preguntas=p;
+        this.materia=m;
+        this.termino=t;
+    }
+}
