f581095af5a0576016a1a7db14e880d98f063677
lolothens-e <annacast@espol.edu.ec>
2023-07-26 21:55:03 -0500

    Implementacion de metodos para archivo (prueba) (Hablar con tutor)

--- proyecto1_grupo07/src/main/java/modelo/Materia.java
+++ proyecto1_grupo07/src/main/java/modelo/Materia.java
@@ -1,7 +1,8 @@
 package modelo;
+import java.io.*;
 import java.util.*;
 
-public class Materia {
+public class Materia implements Serializable{
     private String codigo;
     private String nombre;
     private int cantNiveles;
@@ -57,6 +58,58 @@ public class Materia {
             return false;
         }
         return Objects.equals(this.nombre, other.nombre);
+    }
+    public static void main(String[] args) {
+        for(Materia m: cargarMateriasseria("archivo/Materia.type")){
+            System.out.print(m.toString());
+            
+        }
+    }
+    public static ArrayList<Materia> cargarMateriasarchi(String path){
+        ArrayList<Materia> lfinal=new ArrayList<>();
+        try{
+            BufferedWriter writer=new BufferedWriter(new FileWriter(path,true));
+            writer.write("CCPG1000,ALGEBRA LINEAL,2");
+            writer.newLine();
+            writer.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        try{
+            BufferedReader reader=new BufferedReader(new FileReader(path));
+            String line="";
+            while((line=reader.readLine())!=null){
+                String[] elementos=line.split(",");
+                lfinal.add(new Materia(elementos[0], elementos[1], Integer.valueOf(elementos[2])));
+            }
+            reader.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return lfinal;
+    }
+    public static ArrayList<Materia> cargarMateriasseria(String path){
+        ArrayList<Materia> lfinal=new ArrayList<>();
+        try{
+            ObjectOutputStream out=new ObjectOutputStream(new FileOutputStream(path,true));
+            out.writeObject(new Materia("CCPG1000", "ALGEBRA LINEAL",2));
+            out.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        try{
+            ObjectInputStream in=new ObjectInputStream(new FileInputStream(path));
+            Object o=in.readObject();
+            Materia m=(Materia)o;
+            lfinal.add(m);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return lfinal;
     }
     
     
