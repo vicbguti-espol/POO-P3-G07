70fd8ba9157f4a7f15b3f9885083a412aac1f6a5
vicbguti-espol <vicbguti@espol.edu.ec>
2023-08-23 19:33:22 -0500

    Upload jar file

---
+++ proyecto1_grupo07/src/main/java/espol/poo/proyectojar/GUIStarter.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package espol.poo.proyectojar;
+
+/**
+ *
+ * @author Omen
+ */
+public class GUIStarter {
+    public static void main(final String[] args) {
+        App.main(args);
+    }
+}
