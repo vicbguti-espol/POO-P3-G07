334ace521d11e592b6e51919f6cd0453a0d23a16
Jazarumag <118693522+Jazarumag@users.noreply.github.com>
2023-08-23 21:24:59 -0500

    Update App.java

--- proyecto1_grupo07/src/main/java/espol/poo/proyectojar/App.java
+++ proyecto1_grupo07/src/main/java/espol/poo/proyectojar/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.scene.image.Image;
 import modelo.academico.TerminoAcademico;
 
 import modelo.juego.Juego;
